dfe4b17884caf2be8274b4046a0148d5c87fa1df
tobschoe <66690967+tobschoe@users.noreply.github.com>
2020-08-05 00:46:53 +0200

    Add files via upload

--- js/shutdown.js
+++ js/shutdown.js
@@ -1,5 +1,7 @@
 var exec = require('child_process').exec;
 var config = require('../config/config')
+var os = require('os');
+const { O_DIRECT } = require('constants');
 
 module.exports = function (callback){
   exec(config.shutdownCmd, function(error, stdout, stderr){ callback(stdout); });
