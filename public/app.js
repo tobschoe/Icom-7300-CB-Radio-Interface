8652e95ae2ae3bbb3b65747f056c93ef7abfca60
tobschoe <66690967+tobschoe@users.noreply.github.com>
2020-08-04 15:58:00 +0200

    Add files via upload
    cleanup

--- public/app.js
+++ public/app.js
@@ -1,50 +1,45 @@
 var socket = io()
-const FM = document.getElementById('FM')
-const AM = document.getElementById('AM')
-const USB = document.getElementById('USB')
-const LSB = document.getElementById('LSB')
-var line = 0
 var currentChannel = 0
 const chnAr = ['0', '26.965.00', '26.975.00', '26.985.00', '27.005.00', '27.015.00', '27.025.00', '27.035.00', '27.055.00', '27.065.00', '27.075.00', '27.085.00',
   '27.105.00', '27.115.00', '27.125.00', '27.135.00', '27.155.00', '27.165.00', '27.175.00', '27.185.00', '27.205.00', '27.215.00', '27.225.00', '27.255.00', '27.235.00',
   '27.245.00', '27.265.00', '27.275.00', '27.285.00', '27.295.00', '27.305.00', '27.315.00', '27.325.00', '27.335.00', '27.345.00', '27.355.00', '27.365.00', '27.375.00',
   '27.385.00', '27.395.00', '27.405.00']
 
-$('#FM').click(function (e) {
+$('#FM').click(function () {
   socket.emit('change mod', 'FM')
 })
-$('#AM').click(function (e) {
+$('#AM').click(function () {
   socket.emit('change mod', 'AM')
 })
-$('#USB').click(function (e) {
+$('#USB').click(function () {
   socket.emit('change mod', 'USB')
 })
-$('#LSB').click(function (e) {
+$('#LSB').click(function () {
   socket.emit('change mod', 'LSB')
 })
-$('#command').submit(function (e) {
+$('#command').submit(function () {
   console.log(document.getElementById('cmdID').value)
   e.preventDefault()
   socket.emit('command', document.getElementById('cmdID').value)
   $('#cmdID').innerText = ''
 })
 
-$('#minus').click(function (e) {
+$('#minus').click(function () {
   if (document.getElementById('frq').value != '1') {
     socket.emit('minus', document.getElementById('frq').value)
   }
 })
-$('#plus').click(function (e) {
+$('#plus').click(function () {
   if (document.getElementById('frq').value != '40') {
     socket.emit('plus', document.getElementById('frq').value)
   }
 })
 
-$('#chnOverlayBtn').click(function (e) {
+$('#chnOverlayBtn').click(function () {
   document.getElementById('overlayWindow').style.visibility = 'visible'
 })
 
-$('#oEnter').click(function (e) {
+$('#oEnter').click(function () {
   if (document.getElementById('chnOTextarea').value != undefined) {
     var chnValue = parseInt(document.getElementById('chnOTextarea').value)
     if (chnValue <= 0 || chnValue >= 41) {
@@ -59,64 +54,64 @@ $('#oEnter').click(function (e) {
   document.getElementById('chnOTextarea').value = ''
 })
 
-$('#bBack').click(function (e) {
+$('#bBack').click(function () {
   document.getElementById('chnOTextarea').value = ''
   document.getElementById('overlayWindow').style.visibility = 'hidden'
 })
 var chnSelectDisplay = document.getElementById('chnOTextarea')
 
-$('#b1').click(function (e) {
+$('#b1').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '1'
   }
 })
-$('#b2').click(function (e) {
+$('#b2').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '2'
   }
 })
-$('#b3').click(function (e) {
+$('#b3').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '3'
   }
 })
-$('#b4').click(function (e) {
+$('#b4').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '4'
   }
 })
-$('#b5').click(function (e) {
+$('#b5').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '5'
   }
 })
-$('#b6').click(function (e) {
+$('#b6').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '6'
   }
 })
-$('#b7').click(function (e) {
+$('#b7').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '7'
   }
 })
-$('#b8').click(function (e) {
+$('#b8').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '8'
   }
 })
-$('#b9').click(function (e) {
+$('#b9').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '9'
   }
 })
-$('#b0').click(function (e) {
+$('#b0').click(function () {
   if (chnSelectDisplay.value.length < 2) {
     document.getElementById('chnOTextarea').value += '0'
   }
 })
 // Delete
-$('#oDelete').click(function (e) {
+$('#oDelete').click(function () {
   document.getElementById('chnOTextarea').value = ''
 })
 
@@ -126,7 +121,6 @@ socket.on('frq', data => {
     for (i = 0; i <= chnAr.length; i++) {
       if (chnAr[i] == data) {
         document.getElementById('chn').value = chnAr.indexOf(data)
-        currentChannel = chnAr.indexOf(data)
         document.getElementById('mainwindow').style.backgroundColor = 'black'
         document.getElementById('warning').style.visibility = 'hidden'
         return
